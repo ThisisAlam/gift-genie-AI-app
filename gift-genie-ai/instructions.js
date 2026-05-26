f08fe0717f60cbf7a90bbad8872455f9f129e7d3
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-05-26 08:14:33 +0500

    updated the codebase

---
+++ gift-genie-ai/instructions.js
@@ -0,0 +1,6 @@
+export const instructions = [
+    {
+        role: "user",
+        content: "suggest me some gift ideas for someone who loves flowers"
+    }
+]
--- gift-genie-ai/src/App.jsx
+++ gift-genie-ai/src/App.jsx
@@ -1,12 +1,60 @@
+// Importing Images
 import titleImg from "./assets/genie.svg"
 import lampImg from "./assets/lamp.svg"
+// Importing React
 import React from 'react'
+// Importing OpenAI
+import OpenAI from "openai"
+// Importing JS Programs
+import { instructions as prompt } from "../instructions.js"
+import { checkEnvironment } from "../utils"
 
 export default function App() {
-  
-  const [input, setInput] = React.useState
+  // Defining State
+  const [input, setInput] = React.useState()
+  const [loadingState, setLoadingState] = React.useState(false)
+  // SetUp OpenAI Client
+  const openai = new OpenAI ({
+    apiKey : import.meta.env.VITE_AI_KEY,
+    baseURL : import.meta.env.VITE_AI_URL,
+    dangerouslyAllowBrowser : true
+  })
+  // Checking Environment
+  checkEnvironment()
 
-  return (
-   <h1>This is genie App</h1>
-  )
+  // CALLING ASYNC AWAIT FUNCTION
+  async function setResponseState(){
+    try{
+      const response = await openai.chat.completions.create({
+        model: import.meta.env.VITE_AI_MODEL,
+        messages: prompt
+      })
+      const responseOutput = response.choices[0].message.content
+      setInput(responseOutput)
+
+    } catch(error){
+      if(error.status === 401 || error.status === 403){
+        setInput("Authentication error: Check your AI-KEY and make sure it's Valid");
+      } else if(error.status >= 500){
+        setInput("AI provider error: Something went wrong on the provider side. Try again shortly.");
+      } else {
+        setInput("Unexpected error:" `${error.message || error}`);
+      }
+
+    } finally {
+      setLoadingState(false)
+    }
+  }
+
+   return(
+    <>
+      <h1>This is genie App</h1>
+      <p>Click to call AI and ask for suggestion</p>
+      <button onClick={setResponseState}>Call AI</button>
+      <div>
+        <p>Prompt: {prompt[0].content}</p>
+        <p>{"Making AI Request..." +<br/>+ input}</p>
+      </div>
+    </>
+   )
 }
